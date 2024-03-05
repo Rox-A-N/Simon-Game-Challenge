41446a7e460544feaea3f4bafb57b731eec6d147
Rox Niederk <rox.niederkorn@gmail.com>
2024-03-04 23:12:07 -0600

    Creating the game Pattern with random number generated to pick the color index to be next in the pattern

---
+++ game.js
@@ -0,0 +1,17 @@
+var buttonColor = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+
+
+function nextSequence() {
+// randomNumber will be used to randomly select the index number of buttonColor
+    var randomNumber = Math.floor(Math.random() * 4);
+
+// variable shows the index of the button color randomly chosen
+    var randomChosenColor = buttonColor[randomNumber];
+
+// Adds the new randomChosenColor generated above to the end of the gamePattern
+    gamePattern.push(randomChosenColor);
+
+
+}
